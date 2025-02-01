a877884c79333023b86c1cce876897da182d1a5e
Ubaid Ali <ubaidrashid256@gmail.com>
2025-02-01 12:59:10 +0500

    Add files via upload

---
+++ code alpha task1(gallery).js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+    $("#img1").click(function(){
+        $(".images").css({"display":"none"});
+        $(".button").css({"display":"block"});
+        $(".background").css({"background":"url('1.png') center /cover no-repeat fixed","display":"block"});
+    })
+
+    $("#img2").click(function(){
+        $(".images").css({"display":"none"});
+        $(".button").css({"display":"block"});
+        $(".background").css({"background":"url('2.png') center /cover no-repeat fixed","display":"block"});
+    })
+    $("#img3").click(function(){
+        $(".images").css({"display":"none"});
+        $(".button").css({"display":"block"});
+        $(".background").css({"background":"url('3.png') center /cover no-repeat fixed","display":"block"});
+    })
+    $("#img4").click(function(){
+        $(".images").css({"display":"none"});
+        $(".button").css({"display":"block"});
+        $(".background").css({"background":"url('4.png') center /cover no-repeat fixed","display":"block"});
+    })
+    $("#img5").click(function(){
+        $(".images").css({"display":"none"});
+        $(".button").css({"display":"block"});
+        $(".background").css({"background":"url('5.png') center /cover no-repeat fixed","display":"block"});
+    })
+    $("#img6").click(function(){
+        $(".images").css({"display":"none"});
+        $(".button").css({"display":"block"});
+        $(".background").css({"background":"url('6.png') center /cover no-repeat fixed","display":"block"});
+    })
+    $("#img7").click(function(){
+        $(".images").css({"display":"none"});
+        $(".button").css({"display":"block"});
+        $(".background").css({"background":"url('7.png') center /cover no-repeat fixed","display":"block"});
+    })
+    $("#img8").click(function(){
+        $(".images").css({"display":"none"});
+        $(".button").css({"display":"block"});
+        $(".background").css({"background":"url('8.png') center /cover no-repeat fixed","display":"block"});
+    })
+    $("#img9").click(function(){
+        $(".images").css({"display":"none"});
+        $(".button").css({"display":"block"});
+        $(".background").css({"background":"url('9.png') center /cover no-repeat fixed","display":"block"});
+    })
+    $("#img10").click(function(){
+        $(".images").css({"display":"none"});
+        $(".button").css({"display":"block"});
+        $(".background").css({"background":"url('10.png') center /cover no-repeat fixed","display":"block"});
+    })
+    $("#img11").click(function(){
+        $(".images").css({"display":"none"});
+        $(".button").css({"display":"block"});
+         $(".background").css({"background":"url('11.png') center /cover no-repeat fixed","display":"block"});
+    })
+    $("#img12").click(function(){
+        $(".images").css({"display":"none"});
+        $(".button").css({"display":"block"});
+         $(".background").css({"background":"url('12.png') center /cover no-repeat fixed","display":"block"});
+    })
+})
